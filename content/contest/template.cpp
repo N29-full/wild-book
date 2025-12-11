f99d69e87535a86bce9674c6c59b0bfb195a77ea
N29-full <Noproblem29228@gmail.com>
2025-12-11 13:01:17 +0500

    1

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,15 +1,43 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2")
+
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define trav(a, x) for(auto& a : x)
-#define all(x) x.begin(), x.end()
-#define sz(x) (int)(x).size()
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
 
-int main() {
-	cin.sync_with_stdio(0); cin.tie(0);
-	cin.exceptions(cin.failbit);
+using ll = long long;
+using ld = long double;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+#define all(x)  (x).begin(), (x).end()
+#define sz(x) 	(int)(x).size()
+
+const int N = 1e5+10;
+const int M = 1e5+10;
+const ll INF = (ll)1e18;
+const int mod = 1e9+7;
+const ld eps  = 1e-9;
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // ...
+    return 0;
 }
+/*
+@echo off
+g++ -std=c++17 -o gen.exe gen.cpp
+g++ -std=c++17 -o solution.exe solution.cpp
+g++ -std=c++17 -o brute.exe brute.cpp
+for /l %%x in (1, 1, 1000) do (
+    gen > input.in
+    solution < input.in > output1.out
+    brute < input.in > output2.out
+    fc output1.out output2.out > diagnostics || exit /b
+    echo %%x
+)
+echo norm
+pause
+*/
